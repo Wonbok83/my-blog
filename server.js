a4849de6b3811cbf278d78c6a2e2006ee7030a9b
Wonbok Yang <won-83@hotmail.com>
2018-06-28 21:46:12 -0500

    have server serving

--- server.js
+++ server.js
@@ -2,11 +2,16 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 3001;
-// const path = require("path");
+const path = require("path");
 
 // Use bodyParser in our app
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+
+// This allows us to serve files out of the client/build folder
+app.use(express.static("client/build"));
+
 
 app.get("/", (req, res) => {
     res.send("HI");
@@ -25,16 +30,16 @@ app.post("/api/test", (req, res)=> {
     res.json(req.body);
 });
 
+// This is a catch all if no other routes are matched
+app.use(function(req, res) {
+    res.sendFile(path.join(__dirname, "client/build/index.html"));
+  });
 
-//this allows us to serve files out of the client server
-// app.use(express.static("client/build"));
 
 app.listen(PORT, function() {
     console.log(`API server on port ${PORT}`)
 })
 
-// app.use(function(req, res) {
-//     res.sendFile(path.join(__dirname, "client/build/index.html"));
-//   });
+
 
   
